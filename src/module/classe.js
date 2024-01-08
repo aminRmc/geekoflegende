b21081d08b11315db3cc38ff213d820c1a8becb5
aminRmc <ramcilovicamin@gmail.com>
2024-01-08 11:19:18 +0100

    add set perso guerrier

--- src/module/classe.js
+++ src/module/classe.js
@@ -14,17 +14,28 @@ let Lilith = new Boss("Lilith",180,8)
 
 
 export class Hero {
-    constructor(nom,postureDeCombat,pointDeVie,pointDattaque) {
+    constructor(nom,postureDeCombat,pointDeVie,pointDattaque,specialite) {
         this.nom = nom;
         this.pointDeVie = pointDeVie;
         this.pointDattaque =pointDattaque
         this.postureDeCombat = postureDeCombat
+        this.specialite =specialite
     }
 }
 
 
-let guerrier = new Hero('luffy','attack',150,20)
-let mage = new Hero('Nami','attack',120,10)
+let guerrier = new Hero('luffy','attack',150,20,0)
+let mage = new Hero('Nami','attack',120,10,7)
+let archer = new Hero('Ussop','attack',130,15,6)
 
 
+let setperso = ()=>{
+    NomG = prompt('donnez le nom du guerrier')
+    toLowerCase(Nom) = guerrier.nom
+    PDAG =  prompt('choisie sa position entre attack et defense')
+    toLowerCase(PDA) = guerrier.pointDattaque
 
+
+    
+
+}
