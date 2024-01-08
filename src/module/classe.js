29b109e6ea3f515ae10ac83ef5450abc72117df8
aminRmc <ramcilovicamin@gmail.com>
2024-01-08 09:29:50 +0100

    create class Boss

---
+++ src/module/classe.js
@@ -0,0 +1,14 @@
+export class Boss {
+    constructor(nom,pointDeVie,pointDattaque) {
+        this.nom = nom;
+        this.pointDeVie = pointDeVie;
+        this.pointDattaque =pointDattaque
+
+
+    }
+
+
+}
+
+
+let Boss1 = new Boss()
